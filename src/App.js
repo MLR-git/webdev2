a20ad3a1a1092dc554ab50676c1774a2085656e2
MLR-git <xxxxxx@users.noreply.github.co>
2022-11-26 16:38:24 -0500

    modified:   public/index.html     modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [color, setColor] = React.useState(JSON.parse(sessionStorage.getItem('color')) || "blue");
+  const [count, setCount] = React.useState(JSON.parse(localStorage.getItem('count')) || 0);
+  
+  React.useEffect(() => {
+      sessionStorage.setItem('color', JSON.stringify(color));
+      localStorage.setItem('count', JSON.stringify(count));
+  }, [color, count]);
+
+  return (<div>
+      <DisplayMessage color={color}/>
+      <Clock time={new Date().toLocaleTimeString()} />
+      <p>{count}</p>
+      <button onClick={() => {setColor(toggle(color)); setCount(count+1)}}>Click me</button>
+  </div>
   );
+  
+}
+
+function DisplayMessage(props) {
+return <h1 style={{color:props.color}}>Hello React World - Storage</h1>;
+}
+function Clock(props) {
+return <p>React Clock: {props.time} </p>
+}
+function toggle(color){
+  if (color === "blue"){
+      return "red";
+  }
+  else{
+      return "blue";
+  }
 }
 
 export default App;
