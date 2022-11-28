69a6ae0f75b00fc37c9426e6504fb945d7df77fc
MLR-git <MLR-git@users.noreply.github.com>
2022-11-28 10:44:36 -0500

    backend

---
+++ index.js
@@ -0,0 +1,17 @@
+//calling packages needed for the back-end
+const express = require("express"); //express
+const bodyParser = require("body-parser"); //body-parser -> populates middleware (helps connect front- & back-end)
+const cors = require("cors"); //cross-origin resource sharing
+
+const app = express();
+
+const port = 3001; // Must be different than the port of the React app
+
+app.use(cors()); // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+app.use(express.json()); // Allows express to read a request body
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
